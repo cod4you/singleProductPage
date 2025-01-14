676726f70b50df6ddf97452887db1f241e5e8f86
Saiful Alom <yoursaiful41@gmail.com>
2025-01-14 19:02:09 +0600

    AddBorderForGallaryImg

--- js/app.js
+++ js/app.js
@@ -1,32 +1,3 @@
-// const defultImg = document.querySelector('.container .gallary .defualt img');
-// const firstImg = document.querySelector('.container .gallary .mult-gall .one img');
-// const seconttImg = document.querySelector('.container .gallary .mult-gall .two img');
-// const thirdImg = document.querySelector('.container .gallary .mult-gall .three img');
-// const forthImg = document.querySelector('.container .gallary .mult-gall .four img');
-
-// const firstImgPath = "./img/product1/img-1.png";
-// const seconttImgPath = "./img/product1/img-2.png";
-// const thirdImgPath = "./img/product1/img-3.png";
-// const forthImgPath = "./img/product1/img-4.png";
-
-// firstImg.addEventListener("click", () => {
-//     defultImg.src = firstImgPath;
-
-// })
-// seconttImg.addEventListener("click", () => {
-//     defultImg.src = seconttImgPath;
-
-
-// })
-// thirdImg.addEventListener("click", () => {
-//     defultImg.src = thirdImgPath;
-
-// })
-// forthImg.addEventListener("click", () => {
-//     defultImg.src = forthImgPath;
-
-
-// })
 
 
 const defaultImg = document.querySelector('.container .gallary .defualt img');
@@ -37,6 +8,19 @@ galleryThumbnails.forEach((thumbnail) => {
         defaultImg.src = thumbnail.src;
     });
 });
+
+const allImgDiv = document.querySelectorAll('.mult-gall div');
+allImgDiv.forEach(thumbnail =>{
+    thumbnail.addEventListener('click',() =>{
+
+        allImgDiv.forEach(div => div.style.border = "none")
+      thumbnail.style.border = "3px solid  #ff8822";
+    })
+})
+
+
+
+
 
 
 // for counter
